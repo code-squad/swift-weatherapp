ef93de5abb3810b5e66bb0b1567d31f84bb74566
Dongmin.yoon <ehdals8965@naver.com>
2019-03-26 15:46:31 +0900

    Complete DateTimeCell Setiing

--- AddressBookApp/AddressBookApp/TableViewController.swift
+++ AddressBookApp/AddressBookApp/TableViewController.swift
@@ -15,24 +15,40 @@ class TableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        initialConfigure()
+        initializeConstraint()
+        initaializLabelSetting()
+        setLabelDateAndTime()
     }
     
-    private func initialConfigure() {
+    private func initializeConstraint() {
         dateLabel.translatesAutoresizingMaskIntoConstraints = false
-        eventLabel.translatesAutoresizingMaskIntoConstraints = false
         dateLabel.widthAnchor.constraint(equalTo: timeLabel.widthAnchor, multiplier: 1.0).isActive = true
         dateLabel.trailingAnchor.constraint(equalTo: timeLabel.leadingAnchor).isActive = true
+        eventLabel.translatesAutoresizingMaskIntoConstraints = false
         eventLabel.topAnchor.constraint(equalTo: dateLabel.bottomAnchor).isActive = true
-        
+    }
+    
+    private func initaializLabelSetting() {
         dateLabel.textAlignment = .left
         timeLabel.textAlignment = .right
         eventLabel.textAlignment = .center
         eventLabel.textColor = UIColor.gray
+        dateLabel.font = UIFont.boldSystemFont(ofSize: 17)
         eventLabel.font.withSize(14)
+        eventLabel.text = "새로운 이벤트가 없습니다."
+    }
+    
+    private func setLabelDateAndTime() {
+        let today = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ko_kr")
         
-        dateLabel.backgroundColor = UIColor.yellow
-        timeLabel.backgroundColor = UIColor.red
-        eventLabel.backgroundColor = UIColor.blue
+        dateFormatter.dateFormat = "yyyy년 MM월 dd일"
+        let stringDate = dateFormatter.string(from: today)
+        dateLabel.text = stringDate
+        
+        dateFormatter.dateFormat = "a hh시 mm분"
+        let stringTime = dateFormatter.string(from: today)
+        timeLabel.text = stringTime
     }
 }
