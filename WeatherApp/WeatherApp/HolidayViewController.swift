f1de6141c7bd372fd27ac5e0b271d4bca40561b7
hngfu <djsl1018@naver.com>
2019-03-28 12:13:53 +0900

    [HolidayViewController] 분석한 JSON값을 토대로 테이블뷰가 나타나도록 구현

--- WeatherApp/WeatherApp/HolidayViewController.swift
+++ WeatherApp/WeatherApp/HolidayViewController.swift
@@ -15,7 +15,7 @@ class HolidayViewController: UIViewController {
     
     @IBOutlet weak var holidayTableView: UITableView!
     
-    private var holidays = [Holiday]()
+    private var holidays = [[String: String]]()
     
     private let defaultCell = "defaultCell"
     
@@ -23,6 +23,22 @@ class HolidayViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let strData = """
+                    [{"date":"1월1일", "subtitle":"신정"},
+                    {"date":"2월16일", "subtitle":"구정"},
+                    {"date":"3월1일", "subtitle":"삼일절"},
+                    {"date":"5월5일", "subtitle":"어린이날"},
+                    {"date":"5월22일", "subtitle":"석가탄신일"},
+                    {"date":"6월6일", "subtitle":"현충일"},
+                    {"date":"8월15일", "subtitle":"광복절"},
+                    {"date":"9월24일", "subtitle":"추석"},
+                    {"date":"10월3일", "subtitle":"개천절"},
+                    {"date":"10월9일", "subtitle":"한글날"},
+                    {"date":"12월25일", "subtitle":"성탄절"}]
+                    """
+        guard let data = strData.data(using: String.Encoding.utf8, allowLossyConversion: false),
+            let jsonData = try? JSONSerialization.jsonObject(with: data) as? Array<Dictionary<String,String>> else { return }
+        holidays += jsonData
     }
     
 
@@ -45,6 +61,10 @@ extension HolidayViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: defaultCell, for: indexPath)
+        
+        let holiday = self.holidays[indexPath.row]
+        cell.textLabel?.text = holiday["date"]
+        cell.detailTextLabel?.text = holiday["subtitle"]
         
         return cell
     }
