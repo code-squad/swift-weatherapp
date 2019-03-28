4f8cd4d76bcfbc8907f9d99a25fc59bd461faee4
hngfu <djsl1018@naver.com>
2019-03-28 16:39:10 +0900

    [HolidayTableViewCell] 추가 후 스토리보드 홀리데이테이블뷰에 새로운 셀을 올리고 그 셀의 커스텀클래스로 지정

---
+++ WeatherApp/WeatherApp/HolidayTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HolidayTableViewCell.swift
+//  WeatherApp
+//
+//  Created by 조재흥 on 19. 3. 28..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import UIKit
+
+class HolidayTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
