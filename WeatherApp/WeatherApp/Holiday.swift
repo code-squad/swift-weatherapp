ac9287478295c6db218308cf372ecc211516520d
hngfu <djsl1018@naver.com>
2019-03-28 11:41:04 +0900

    [Holiday] 생성, [HolidayViewController] dataSource 구현

---
+++ WeatherApp/WeatherApp/Holiday.swift
@@ -0,0 +1,14 @@
+//
+//  Holiday.swift
+//  WeatherApp
+//
+//  Created by 조재흥 on 19. 3. 28..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import Foundation
+
+struct Holiday {
+    var date: String
+    var subtitle: String
+}
