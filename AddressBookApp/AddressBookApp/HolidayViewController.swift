22cc38d8fcf166a81e813f4c54f89fc683007726
kiljoonho <kiljh@me.com>
2019-08-18 14:18:45 +0900

    string 을 jsonObject 로 변환하고 tableView 에 뿌려주도록 구현

--- AddressBookApp/AddressBookApp/HolidayViewController.swift
+++ AddressBookApp/AddressBookApp/HolidayViewController.swift
@@ -8,23 +8,38 @@
 
 import UIKit
 
-class HolidayViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class HolidayViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    let str = "[{\"date\":\"1월1일\", \"subtitle\":\"신정\"}, {\"date\":\"2월16일\", \"subtitle\":\"구정\"}, {\"date\":\"3월1일\", \"subtitle\":\"삼일절\"}, {\"date\":\"5월5일\", \"subtitle\":\"어린이날\"}, {\"date\":\"5월22일\", \"subtitle\":\"석가탄신일\"}, {\"date\":\"6월6일\", \"subtitle\":\"현충일\"}, {\"date\":\"8월15일\", \"subtitle\":\"광복절\"}, {\"date\":\"9월24일\", \"subtitle\":\"추석\"}, {\"date\":\"10월3일\", \"subtitle\":\"개천절\"}, {\"date\":\"10월9일\", \"subtitle\":\"한글날\"}, {\"date\":\"12월25일\", \"subtitle\":\"성탄절\"}]"
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let data = str.data(using: .utf8) else {
+            return 0
+        }
+        
+        let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [[String: String]]
+    
+        return json?.count ?? 0
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        guard let data = str.data(using: .utf8) else {
+            return cell
+        }
+        
+        let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [[String: String]]
+        cell.textLabel?.text = json?[indexPath.row]["date"]
+        cell.detailTextLabel?.text = json?[indexPath.row]["subtitle"]
+        
+        return cell
     }
-    */
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
 }
