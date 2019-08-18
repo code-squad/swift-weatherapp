de5335c2fb224c30acdbd6aa50a8fa41721f4fa8
kiljoonho <kiljh@me.com>
2019-08-18 12:58:24 +0900

    HolidayViewController 파일 생성

---
+++ AddressBookApp/AddressBookApp/HolidayViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HolidayViewController.swift
+//  AddressBookApp
+//
+//  Created by joon-ho kil on 8/18/19.
+//  Copyright © 2019 길준호. All rights reserved.
+//
+
+import UIKit
+
+class HolidayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
