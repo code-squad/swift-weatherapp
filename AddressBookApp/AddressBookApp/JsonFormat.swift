791752039a3b753fa920942addb8baa408a0329b
kiljoonho <kiljh@me.com>
2019-08-20 11:03:59 +0900

    Json 타입의 포멧을 저장할 열거형 구현

---
+++ AddressBookApp/AddressBookApp/JsonFormat.swift
@@ -0,0 +1,13 @@
+//
+//  jsonFormat.swift
+//  AddressBookApp
+//
+//  Created by joon-ho kil on 8/20/19.
+//  Copyright © 2019 길준호. All rights reserved.
+//
+
+import Foundation
+
+enum JsonFormat: String {
+    case date, subtitle
+}
