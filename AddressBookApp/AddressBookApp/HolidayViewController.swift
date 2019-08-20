94230c50881a8b799ca065a21281960ebf5c5fbe
kiljoonho <kiljh@me.com>
2019-08-20 11:04:35 +0900

    구현한 모델과 열거형을 사용하도록 코드 수정

--- AddressBookApp/AddressBookApp/HolidayViewController.swift
+++ AddressBookApp/AddressBookApp/HolidayViewController.swift
@@ -9,28 +9,22 @@
 import UIKit
 
 class HolidayViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    let str = "[{\"date\":\"1월1일\", \"subtitle\":\"신정\"}, {\"date\":\"2월16일\", \"subtitle\":\"구정\"}, {\"date\":\"3월1일\", \"subtitle\":\"삼일절\"}, {\"date\":\"5월5일\", \"subtitle\":\"어린이날\"}, {\"date\":\"5월22일\", \"subtitle\":\"석가탄신일\"}, {\"date\":\"6월6일\", \"subtitle\":\"현충일\"}, {\"date\":\"8월15일\", \"subtitle\":\"광복절\"}, {\"date\":\"9월24일\", \"subtitle\":\"추석\"}, {\"date\":\"10월3일\", \"subtitle\":\"개천절\"}, {\"date\":\"10월9일\", \"subtitle\":\"한글날\"}, {\"date\":\"12월25일\", \"subtitle\":\"성탄절\"}]"
+    let model = Model()
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let data = str.data(using: .utf8) else {
-            return 0
-        }
-        
-        let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [[String: String]]
-    
-        return json?.count ?? 0
+        return model.getCellCount()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
-        guard let data = str.data(using: .utf8) else {
-            return cell
-        }
+        model.makeTextLabel(index: indexPath.row, handler: { (value)  in
+            cell.textLabel?.text = value
+        })
         
-        let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [[String: String]]
-        cell.textLabel?.text = json?[indexPath.row]["date"]
-        cell.detailTextLabel?.text = json?[indexPath.row]["subtitle"]
+        model.makeDetailTextLabel(index: indexPath.row, handler: { (value)  in
+            cell.detailTextLabel?.text = value
+        })
         
         return cell
     }
