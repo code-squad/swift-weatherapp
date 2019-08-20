3f481740422afdc07b1f6fcb23f86686f3aee7bf
kiljoonho <kiljh@me.com>
2019-08-20 11:03:33 +0900

    셀을 만들 수 있는 프로토콜 구현

---
+++ AddressBookApp/AddressBookApp/MakeableCell.swift
@@ -0,0 +1,15 @@
+//
+//  drawableCell.swift
+//  AddressBookApp
+//
+//  Created by joon-ho kil on 8/20/19.
+//  Copyright © 2019 길준호. All rights reserved.
+//
+
+import Foundation
+
+protocol MakeableCell {
+    func getCellCount() -> Int
+    func makeTextLabel(index: Int, handler: (String) -> ())
+    func makeDetailTextLabel(index: Int, handler: (String) -> ())
+}
