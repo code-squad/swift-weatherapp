6f96419ca44bd45bc8c9555f27881e1f57b471a0
kiljoonho <kiljh@me.com>
2019-08-20 12:43:28 +0900

    url 을 통해 json String 을 가져오도록 수정

--- AddressBookApp/AddressBookApp/Model.swift
+++ AddressBookApp/AddressBookApp/Model.swift
@@ -9,14 +9,19 @@
 import Foundation
 
  struct Model: MakeableCell {
-    private let str = "[{\"date\":\"1월1일\", \"subtitle\":\"신정\"}, {\"date\":\"2월16일\", \"subtitle\":\"구정\"}, {\"date\":\"3월1일\", \"subtitle\":\"삼일절\"}, {\"date\":\"5월5일\", \"subtitle\":\"어린이날\"}, {\"date\":\"5월22일\", \"subtitle\":\"석가탄신일\"}, {\"date\":\"6월6일\", \"subtitle\":\"현충일\"}, {\"date\":\"8월15일\", \"subtitle\":\"광복절\"}, {\"date\":\"9월24일\", \"subtitle\":\"추석\"}, {\"date\":\"10월3일\", \"subtitle\":\"개천절\"}, {\"date\":\"10월9일\", \"subtitle\":\"한글날\"}, {\"date\":\"12월25일\", \"subtitle\":\"성탄절\"}]"
-    
+    private let url = URL(string: "http://public.codesquad.kr/jk/weatherapp/customcell.json")!
     private let json: [[String: String]]
     
     init() {
-        if let data = str.data(using: .utf8), let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [[String: String]] {
-            self.json = json
-            return
+        do {
+            let jsonString = try String(contentsOf: url)
+            
+            if let data = jsonString.data(using: .utf8), let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [[String: String]] {
+                self.json = json
+                return
+            }
+        } catch {
+            print("error")
         }
         
         self.json = []
