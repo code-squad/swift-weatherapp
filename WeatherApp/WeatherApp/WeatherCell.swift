0f02c511d26d988ea144295ca86e87926dba80e0
hwj0623 <hwj0623@naver.com>
2019-08-27 19:11:13 +0900

    WeatherCell 내부 컴포넌트 설정 및 오토레이아웃 추가

---
+++ WeatherApp/WeatherApp/WeatherCell.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherCell.swift
+//  WeatherApp
+//
+//  Created by hw on 27/08/2019.
+//  Copyright © 2019 hwj. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
