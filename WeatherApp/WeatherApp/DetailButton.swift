5cbd9c989f9b482435cf481b5e5d9740c2683e5b
hwj0623 <hwj0623@naver.com>
2019-08-27 19:11:13 +0900

    WeatherCell 내부 컴포넌트 설정 및 오토레이아웃 추가

---
+++ WeatherApp/WeatherApp/DetailButton.swift
@@ -0,0 +1,21 @@
+//
+//  DetailButton.swift
+//  WeatherApp
+//
+//  Created by hw on 27/08/2019.
+//  Copyright © 2019 hwj. All rights reserved.
+//
+
+import UIKit
+
+class DetailButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
