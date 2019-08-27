0541f4453b988137b87e7145b92d6bacb52d367a
hwj0623 <hwj0623@naver.com>
2019-08-27 18:51:29 +0900

    상단 셀의 날짜/시간 Label 오토레이아웃 추가 - Assets에 이미지 추가

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by hw on 27/08/2019.
+//  Copyright © 2019 hwj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
