2aed6b73279dffa74447eed5d344fd2db9014abf
hwj0623 <hwj0623@naver.com>
2019-08-30 17:01:49 +0900

    공휴일 리스트 테이블 뷰 셀에 데이터 바인딩 구현 - 이전스텝 진행 내역 중 미구현 클래스인 DetailButton 삭제 - TableCell을 별도 클래스파일로 분리 - extension으로 UITableViewDataSource의 메서드 구현사항 분리 - 스토리보드에서 dataSource와 delegate 설정

---
+++ WeatherApp/WeatherApp/TableCell.swift
@@ -0,0 +1,14 @@
+//
+//  TableCell.swift
+//  WeatherApp
+//
+//  Created by hw on 30/08/2019.
+//  Copyright © 2019 hwj. All rights reserved.
+//
+
+import UIKit
+
+class TableCell: UITableViewCell {
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var subtitle: UILabel!
+}
