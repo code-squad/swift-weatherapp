eb435df824a76e1c64f175ff4f54dbe9ef91bd86
hwj0623 <hwj0623@naver.com>
2019-08-30 14:51:23 +0900

    fix : 불필요한 주석 제거

---
+++ WeatherApp/WeatherApp/HolidayViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HolidayViewController.swift
+//  WeatherApp
+//
+//  Created by hw on 30/08/2019.
+//  Copyright © 2019 hwj. All rights reserved.
+//
+
+import UIKit
+
+class HolidayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
