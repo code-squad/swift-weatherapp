f206fdb615f5948f4889830d9dd0f211decf4cf1
hwj0623 <hwj0623@naver.com>
2019-08-31 15:00:44 +0900

    fix : 뷰 컨트롤러 테이블 뷰 세팅작업 클로저로 전달하도록 변경 - 모델 객체를 앱 실행시 주입받도록 변경

--- WeatherApp/WeatherApp/AppDelegate.swift
+++ WeatherApp/WeatherApp/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        guard let vc = window?.rootViewController as? HolidayViewController else { return false }
+        vc.configureModel(holidayList: HolidayList.init())
         return true
     }
 
