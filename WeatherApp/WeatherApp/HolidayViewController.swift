924a366639c6db94e476fa0f7a823da38c53befd
hwj0623 <hwj0623@naver.com>
2019-08-31 15:00:44 +0900

    fix : 뷰 컨트롤러 테이블 뷰 세팅작업 클로저로 전달하도록 변경 - 모델 객체를 앱 실행시 주입받도록 변경

--- WeatherApp/WeatherApp/HolidayViewController.swift
+++ WeatherApp/WeatherApp/HolidayViewController.swift
@@ -9,62 +9,38 @@
 import UIKit
 
 class HolidayViewController: UIViewController {
-    let jsonString = """
-        [{"date":"1월1일", "subtitle":"신정"},
-        {"date":"2월16일", "subtitle":"구정"},
-        {"date":"3월1일", "subtitle":"삼일절"},
-        {"date":"5월5일", "subtitle":"어린이날"},
-        {"date":"5월22일", "subtitle":"석가탄신일"},
-        {"date":"6월6일", "subtitle":"현충일"},
-        {"date":"8월15일", "subtitle":"광복절"},
-        {"date":"9월24일", "subtitle":"추석"},
-        {"date":"10월3일", "subtitle":"개천절"},
-        {"date":"10월9일", "subtitle":"한글날"},
-        {"date":"12월25일", "subtitle":"성탄절"}]
-    """
-    let titleKey = "date"
-    let subtitleKey = "subtitle"
-    var holidayInfoDictionaryArray = [[String: String]]()
+    private var holidayList: HolidayList!
     @IBOutlet weak var holidayTableView: UITableView!
+    
+    func configureModel(holidayList: HolidayList) {
+        self.holidayList = holidayList
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        initiateHolidayInformation()
-    }
-    
-    private func initiateHolidayInformation(){
-        guard let jsonData = jsonString.data(using: .utf8) else {
-            return
-        }
-        guard let result = convertJsonToDictionaryArray(jsonData) else {
-            return
-        }
-        holidayInfoDictionaryArray = result
-    }
-    
-    private func convertJsonToDictionaryArray(_ data: Data) -> [[String:String]]? {
-        guard let object = try? JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
-            as? [[String: String]] else {
-            return nil
-        }
-        return object
     }
 }
 
 extension HolidayViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return holidayInfoDictionaryArray.count
+        let format = { (count: Int) in
+            return count
+        }
+        let count = holidayList.receiveTableViewCountFormat(format: format)
+        return  count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell  = tableView.dequeueReusableCell(withIdentifier: "TableCell") as? TableCell else {
             return TableCell()
         }
-        guard let title = holidayInfoDictionaryArray[indexPath.row][titleKey],
-                let subtitle = holidayInfoDictionaryArray[indexPath.row][subtitleKey] else {
-            return cell
+        let format = { (date: String, subtitle: String ) in
+            cell.title.text = date
+            cell.subtitle.text = subtitle
         }
-        cell.title.text = title
-        cell.subtitle.text = subtitle
+        holidayList.receiveTableViewContentFormat(format: format, rowAt: indexPath.row)
+
+        
         return cell
     }
 }
