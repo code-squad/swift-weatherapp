ec38d5142c05673ad0049e493fa3f5bad7bfafde
hwj0623 <hwj0623@naver.com>
2019-09-02 16:35:41 +0900

    커스템 셀 생성 및 오토레이아웃 설정

---
+++ WeatherApp/WeatherApp/HolidayTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  HolidayTableViewCell.swift
+//  WeatherApp
+//
+//  Created by hw on 02/09/2019.
+//  Copyright © 2019 hwj. All rights reserved.
+//
+
+import UIKit
+
+class HolidayTableViewCell: UITableViewCell {
+ 
+//    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+}
