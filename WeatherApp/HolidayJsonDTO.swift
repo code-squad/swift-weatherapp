76e443a872256ff025683e02ef3d9f92fbea7b7c
hwj0623 <hwj0623@naver.com>
2019-09-03 13:17:41 +0900

    fix : 중복 메서드 삭제

--- WeatherApp/HolidayJsonDTO.swift
+++ WeatherApp/HolidayJsonDTO.swift
@@ -11,24 +11,13 @@ import Foundation
 struct HolidayJsonDTO {
     private var data: Data = Data.init()
     
-    static func receiveCorrectData() -> Data? {
-        let holidayURL = URL.init(string: "http://public.codesquad.kr/jk/weatherapp/customcell.json")!
-        do {
-            let holidayDataString = try String(contentsOf: holidayURL, encoding: .utf8)
-            guard let jsonData = holidayDataString.data(using: .utf8) else {
-                return nil
-            }
-            return jsonData
-        }catch {
-            
-            let result = ["result" : error ]
-            NotificationCenter.default.post(name: .networkError, object: nil, userInfo: result)
+    static func receiveJsonData(_ flag: Bool) -> Data?{
+        var holidayURL: URL
+        if flag {
+            holidayURL = URL.init(string: "http://public.codesquad.kr/jk/weatherapp/customcell.json")!
+        }else {
+            holidayURL = URL.init(string: "http://public.codesquad.kr/jk/weatherapp/customce")!
         }
-        return nil
-    }
-    
-    static func receiveJsonData() -> Data?{
-        let holidayURL = URL.init(string: "http://public.codesquad.kr/jk/weatherapp/customce")!
         do {
             let holidayDataString = try String(contentsOf: holidayURL, encoding: .utf8)
             guard let jsonData = holidayDataString.data(using: .utf8) else {
