b0c5a1efe816bf6bbcfae95332b3a6d7755861c3
부엉이 <o.o.wl.soft@gmail.com>
2019-11-19 04:08:52 +0900

    feat:  git ignore  CocoaPod 설정

---
+++ WeatherApp/WeatherApp/Common/Reuseable.swift
@@ -0,0 +1,14 @@
+//
+//  Reuseable+Cell.swift
+//  WeatherApp
+//
+//  Created by 이동영 on 2019/11/19.
+//  Copyright © 2019 이동영. All rights reserved.
+//
+
+import UIKit
+
+protocol Reuseable {
+    
+    static var reuseIdentifier: String { get }
+}
---
+++ WeatherApp/WeatherApp/Common/WeatherAsset.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherAsset.swift
+//  WeatherApp
+//
+//  Created by 이동영 on 2019/11/19.
+//  Copyright © 2019 이동영. All rights reserved.
+//
+
+import Foundation
