2eca343c5a8466f9aa138f9a3ea3d55cf38ca0ad
부엉이 <o.o.wl.soft@gmail.com>
2019-11-19 04:11:46 +0900

    feat: TableView + Reuseable 을 준수하는 Cell 에대한 dequeue 메소드 확장

--- WeatherApp/WeatherApp/Extension/TableView+.swift
+++ WeatherApp/WeatherApp/Extension/TableView+.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2019 이동영. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UITableView {
+    
+    func dequeueReusableCell<Cell: Reuseable>(withType cellType: Cell.Type,
+                                              for indexPath: IndexPath) -> Cell? {
+        let cell = self.dequeueReusableCell(withIdentifier: cellType.reuseIdentifier, for: indexPath)
+        
+        return cell as? Cell
+    }
+}
+
