611a9e27803d633ecf8fce7ac46b34e881feb94d
부엉이 <o.o.wl.soft@gmail.com>
2019-11-19 11:08:30 +0900

    feat: TableView Date Source 구현

--- WeatherApp/WeatherApp/WeatherViewController.swift
+++ WeatherApp/WeatherApp/WeatherViewController.swift
@@ -6,21 +6,17 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class WeatherViewController: UITableViewController {
-
+    
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        configureTableView()
     }
-
-    // MARK: - Table view data source
-
     
     // MARK: - Configure
     private func configureTableView() {
@@ -33,69 +29,51 @@ class WeatherViewController: UITableViewController {
                         forCellReuseIdentifier: WeatherCell.reuseIdentifier)
         }
     }
+}
+// MARK: - Layout & Attribute
+extension WeatherViewController {
+    
+}
+// MARK: - Table view data source
+extension WeatherViewController {
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 2
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        switch indexPath.row {
+        case 0:
+            return getDateTimeCell(for: indexPath)
+        case 1:
+            return getWeatherCell(for: indexPath)
+        default:
+            fatalError("TableCell is not configured.")
+        }
+    }
+    
+    private func getDateTimeCell(for indexPath: IndexPath) -> DateTimeCell {
+        guard
+            let cell = tableView.dequeueReusableCell(withType: DateTimeCell.self, for: indexPath)
+            else { fatalError("DateTimeCell is not configured.") }
+        
+        cell.configure()
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    private func getWeatherCell(for indexPath: IndexPath) -> WeatherCell {
+        guard
+            let cell = tableView.dequeueReusableCell(withType: WeatherCell.self, for: indexPath)
+            else { fatalError("WeatherCell is not configured.") }
+        
+        cell.configure()
+        
+        return cell
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
