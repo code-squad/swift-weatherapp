5e5df08fc5e740d5d6d3da507c2bb359d10bea01
부엉이 <o.o.wl.soft@gmail.com>
2019-11-19 16:51:01 +0900

    feat: HolidayCell 구현

--- WeatherApp/WeatherApp/View/Cell/HolidayCell.swift
+++ WeatherApp/WeatherApp/View/Cell/HolidayCell.swift
@@ -9,16 +9,27 @@
 import UIKit
 
 class HolidayCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    // MARK: - Initializer
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
+    
+    // MARK: - Configure
+    func configure(holiday: Holiday) {
+        self.textLabel?.text = holiday.date
+        self.detailTextLabel?.text = holiday.subtitle
+    }
+    
+}
+// MARK: - Reuseable
+extension HolidayCell: Reuseable {
+    
+    static var reuseIdentifier: String {
+        return "HolidayCell"
+    }
 }
