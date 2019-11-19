def88b9fb33050e2b295c919d1ae911120802e42
부엉이 <o.o.wl.soft@gmail.com>
2019-11-19 15:02:09 +0900

    feat: 현재 날짜를 포맷팅하여 반환하는  CurrentTime 메소드 확장

---
+++ WeatherApp/WeatherApp/View/Cell/HolidayCell.swift
@@ -0,0 +1,24 @@
+//
+//  HolidayCell.swift
+//  WeatherApp
+//
+//  Created by 이동영 on 2019/11/19.
+//  Copyright © 2019 이동영. All rights reserved.
+//
+
+import UIKit
+
+class HolidayCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
