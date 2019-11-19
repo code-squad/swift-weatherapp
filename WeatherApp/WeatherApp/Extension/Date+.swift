d9bb5160e5f28f2616133331e8a01d2782ffc41a
부엉이 <o.o.wl.soft@gmail.com>
2019-11-19 15:02:09 +0900

    feat: 현재 날짜를 포맷팅하여 반환하는  CurrentTime 메소드 확장

---
+++ WeatherApp/WeatherApp/Extension/Date+.swift
@@ -0,0 +1,20 @@
+//
+//  Date+.swift
+//  WeatherApp
+//
+//  Created by 이동영 on 2019/11/19.
+//  Copyright © 2019 이동영. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    
+    static func currentTime(with pattern: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = pattern
+        
+        return dateFormatter.string(from: Date())
+    }
+    
+}
