d381d501fbef95159c0d78c757e2d0768eb5d257
conyconydev <conyconydev@gmail.com>
2019-11-25 13:33:27 +0900

    add : HolidayTableViewCell 클래스 추가

---
+++ WeatherApp/WeatherApp/HolidayTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HolidayTableViewCell.swift
+//  WeatherApp
+//
+//  Created by kwangrae kim on 2019/11/25.
+//  Copyright © 2019 elena. All rights reserved.
+//
+
+import UIKit
+
+class HolidayTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
