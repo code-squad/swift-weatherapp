7dac6e68c00138609503f359d0181ec0b1ed4c55
conyconydev <conyconydev@gmail.com>
2019-11-25 22:36:49 +0900

    add : IBOutlet 추가

--- WeatherApp/WeatherApp/HolidayTableViewCell.swift
+++ WeatherApp/WeatherApp/HolidayTableViewCell.swift
@@ -10,15 +10,12 @@ import UIKit
 
 class HolidayTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    //MARK: - IBOutlet
+    
+    @IBOutlet weak var sibtitleLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var dateLable: UILabel!
+    
+    
 
 }
